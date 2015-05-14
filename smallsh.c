18b02ad68ce3528b2b727abfd438ab614e6e6f17
Brandon Swanson <swanyriver@gmail.com>
2015-05-14 10:51:03 -0700

    input buffer is main function local

--- smallsh.c
+++ smallsh.c
@@ -13,21 +13,22 @@
 #include <string.h>
 
 //GLOBAL CONSTANTS
-#define INBUFFSIZE 512 // chat is requesting much longer that this TODO verify,
+//#define INBUFFSIZE 512 // chat is requesting much longer that this TODO verify,
 //is there a place to put this stream other than on the stack, read it pieces at a time?
 
 //GLOBAL VARIABLES
-char inputBuffer[INBUFFSIZE];
-char** inBuffPointer = inputBuffer;
+const int INBUFFSIZE = 512;
 
 int showPrompt();
 
 int main(void) {
 
+	char inputBuffer[INBUFFSIZE];
+
 	while(strcmp(inputBuffer,"exit") != 0){
 		size_t rec = showPrompt();
-		if(*inBuffPointer != inputBuffer) printf("%s", "input buffer has been malloced");
-		printf("Received {%d} bytes => %s", rec, inputBuffer);
+		//if(*inBuffPointer != inputBuffer) printf("%s", "input buffer has been malloced");
+		//printf("Received {%d} bytes => %s", rec, inputBuffer);
 	}
 
 	return 0;
@@ -36,5 +37,6 @@ int main(void) {
 int showPrompt(){
 	printf("%s","\nsmallSH:");
 	fflush(stdout);
-	return getline(inBuffPointer,INBUFFSIZE,stdin);
+	//return getline(inBuffPointer,INBUFFSIZE,stdin);
+	return 0;
 }
