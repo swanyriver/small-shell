73198dc60bb76e6e3c2f3a9e398b4855d4a6d740
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2015-05-14 12:31:45 -0700

    getline not to be used with stack buffer, cant make pointer to stack cstring, no actuall adress of pointer, its all macro expansion of something, addrof(char array) returns same as char arra

--- smallsh.c
+++ smallsh.c
@@ -19,14 +19,29 @@
 //GLOBAL VARIABLES
 const int INBUFFSIZE = 512;
 
+//TODO can have global char** to local input buffer
+//      unassigned here, assigned in main
+
+
+
+//function prototypes
 int showPrompt();
 
 int main(void) {
 
-	char inputBuffer[INBUFFSIZE];
+	char *inputBuffer = malloc(INBUFFSIZE * sizeof(char));
+
+	strncpy(inputBuffer,"hello",10);
+
+	//printf("addr:%d, *str:%d, &str:%d, first(%d):%c, str:%s",inputBuffer, *inputBuffer, &inputBuffer, *inputBuffer,*(inputBuffer+1), inputBuffer);
+
+	char** strpp = &inputBuffer;
+	printf("firstchar %p,   bufferaddr: %p", inputBuffer, strpp);
+
+	exit(1);
 
 	while(strcmp(inputBuffer,"exit") != 0){
-		size_t rec = showPrompt();
+		int rec = showPrompt();
 		//if(*inBuffPointer != inputBuffer) printf("%s", "input buffer has been malloced");
 		//printf("Received {%d} bytes => %s", rec, inputBuffer);
 	}
