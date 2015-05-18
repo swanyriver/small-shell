82668c6a1cb8b3a9dd07ce8a0735465765433f1d
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2015-05-18 16:57:11 -0700

    the source of the problem was not including string.h

--- prepare.c
+++ prepare.c
@@ -10,13 +10,15 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "prepare.h"
 
 
 
-void _safe_exit(int FDout, int FDin, char* errorMsg){
+void _safe_exit(int FDout, int FDin, const char* errorMsg){
 
     fprintf(stderr,"FILE ERROR:%s\n",errorMsg);
+    fflush(stderr);
 
     if(FDin) close(FDin);
     if(FDout) close(FDout);
@@ -30,16 +32,15 @@ void preprare_redirects(cmd *command){
     int FDin = 0;
     int FDout = 0;
 
+    //_safe_exit(FDin,FDout,"here we are");
+
 	if(command->redirIn){
-
-	    printf("opening input redirect:%s\n",command->inFILE);
-
 	    FDin = open(command->inFILE, O_RDONLY);
 
 	    if(FDin == 0){
 	        _safe_exit(FDin,FDout,"File for input not opened");
 	    } else if (FDin == -1){
-	        _safe_exit(FDin,FDout,(char*) strerror(errno));
+	        _safe_exit(FDin,FDout,strerror(errno));
 	    }
 
 	    //file opened succesfully, redirect stdin
@@ -48,7 +49,6 @@ void preprare_redirects(cmd *command){
 	}
 
 	if(command->redirOut){
-	    printf("opening output redirect:%s\n",command->outFILE);
 	    FDout = open(command->outFILE, O_WRONLY|O_CREAT|O_TRUNC, 0644);
 
 
